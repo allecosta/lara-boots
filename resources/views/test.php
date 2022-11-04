edd59e8fa03bcb12ad5daaa3ea1cd6b51f1f82fe
Alexandre Costa <59491758+allecosta@users.noreply.github.com>
2022-11-04 20:07:30 +0000

    Initial commit

---
+++ resources/views/test.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en";
+<head>
+    <meta charset="utf-8">
+    <title>Larafriday</title>
+
+    <style>
+        html {
+            background-color: #1c2333;
+        }
+        h1 {
+            color: red;
+            text-align: center;
+            font-size: 50px;
+            font-family: cursive;
+        }
+
+        p {
+            text-align: center;
+            color: white;
+            font-family: lucida console;   
+        }
+
+        .container {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        #course {
+          font-family: Arial, Helvetica, sans-serif;
+          border-collapse: collapse;
+          width: 70%;
+            margin-top: 50px;
+        }
+
+        #course td, #course th {
+          border: 1px solid #ddd;
+          padding: 8px;
+        }
+        
+        #course tr:nth-child(even){background-color: white;}
+        
+        #course tr:nth-child(odd){background-color: grey;}
+        
+        #course th {
+          padding-top: 12px;
+          padding-bottom: 12px;
+          text-align: center;
+          background-color: grey;
+          color: white;
+        }
+    </style>
+</head>
+<body>
+    <h1>Larafriday</h1>    
+    <p>Todas as sextas-feiras dedico o meu dia para estudar este maravilhoso e poderoso framework</p>
+
+    <div class="container">
+        <table id="course">
+          	<tr>
+	            <th>Cursos</th>
+	            <th>Link</th>
+	          </tr>
+	          <tr>
+	            <td>Build PHP MVC Framework</td>
+	            <td>
+	                <a href="https://www.youtube.com/watch?v=WKy-N0q3WRo&list=PLLQuc_7jk__Uk_QnJMPndbdKECcTEwTA1">
+	                    https://www.youtube.com/watch?v=WKy-N0q3WRo&list=PLLQuc_7jk__Uk_QnJMPndbdKECcTEwTA1
+	                </a
+	            </td>
+	          </tr>
+	          <tr>
+	            <td>Laravel PHP Framework Tutorial - Beginners</td>
+	            <td>
+					<a href="https://youtu.be/ImtZ5yENzgE">
+						https://youtu.be/ImtZ5yENzgE
+					</a>
+				</td>
+	          </tr>
+	          <tr>
+	            <td>Laravel PHP Framework Tutorial - Full Course</td>
+	            <td>
+					<a href="https://youtu.be/BXiHvgrJfkg">
+						https://youtu.be/BXiHvgrJfkg
+					</a>
+				</td>
+	          </tr>
+	          <tr>
+	            <td>Laravel 9 From Scratch</td>
+	            <td>Roland Mendel</td>
+	          </tr>
+	          <tr>
+	            <td>Island Trading</td>
+	            <td>
+					<a href="https://youtu.be/MYyJ4PuL4pY">
+						https://youtu.be/MYyJ4PuL4pY
+					</a>
+				</td>
+	          </tr>
+	          <tr>
+	            <td>Laravel E-Commerce</td>
+	            <td>
+					<a href="https://www.youtube.com/watch?v=o5PWIuDTgxg&list=PLEhEHUEU3x5oPTli631ZX9cxl6cU_sDaR">
+						https://www.youtube.com/watch?v=o5PWIuDTgxg&list=PLEhEHUEU3x5oPTli631ZX9cxl6cU_sDaR
+					</a>
+				</td>
+	          </tr>
+			<tr>
+				<td>Laravel Movie App</td>
+				<td>
+					<a href="https://www.youtube.com/watch?v=9OKbmMqsREc&list=PLEhEHUEU3x5pYTjZze3fhYMB4Nl_WOHI4">
+						https://www.youtube.com/watch?v=9OKbmMqsREc&list=PLEhEHUEU3x5pYTjZze3fhYMB4Nl_WOHI4
+					</a>
+				</td>
+			</tr>
+			<td>A lot of great videos from Laraveller</td>
+				<td>
+					<a href="https://www.youtube.com/c/Laraveller/videos">
+						https://www.youtube.com/c/Laraveller/videos
+					</a>
+				</td>
+			</tr>
+        </table>
+    </div>
+    
+</body>
+</html>
